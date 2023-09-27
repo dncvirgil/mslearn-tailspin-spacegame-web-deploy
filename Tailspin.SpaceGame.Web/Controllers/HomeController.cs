d0ba5fc043938934fbc7971052a4720010726723
Virgil Danciu <vdanci@softserveinc.com>
2023-09-27 14:14:12 +0300

    Change text and colors on the home page

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TailSpin.SpaceGame.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // High score repository.
+        // High score repository. hh
         private readonly IDocumentDBRepository<Score> _scoreRepository;
         // User profile repository.
         private readonly IDocumentDBRepository<Profile> _profileRespository;
